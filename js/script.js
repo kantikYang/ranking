9e109257a958a38bf376d6755d4e7ed81b1bd87b
Kantemir <mr.kant2001@mail.ru>
2022-03-29 20:16:57 +0300

    Структура данных,вывод и сортировка,добавление юзера

--- js/script.js
+++ js/script.js
@@ -1,73 +1,45 @@
 
 
+
 const form = document.querySelector('.add-form');
 const tbody = document.querySelector('.tbody')
 const table = document.querySelector('.table')
 const bg = document.querySelector('.bg-edit');
 
-let userList = [];
+const userList = [];
 
-let number = 0;
-
-
-//получение данных с формы
-function dataForm(event) {
-  event.preventDefault();
-  const formData = new FormData(form);
-  const user = Object.fromEntries(formData.entries());
-  if (validation(user)) {
-    addUser(user);
-  }
-  else {
-    alert("error");
-  }
-  event.target.reset();
-}
-
-
-//добавление нового игрока
-const addUser = (user) => {
-  userList[number] = user;
-  userList[number].pts = (+userList[number].win + +userList[number].ko) - +userList[number].looses;
-
-  sortTable(userList);
-
-  createUser(userList);
-  number++;
-  console.log(userList);
-}
-
-const validation = (form) => {
-
-  if ((+form.win >= 0) && (+form.ko >= 0) && (+form.looses >= 0) && (form.win >= form.ko) && (form.name.length > 0)) {
-    return true;
-  }
-  else {
-    return false;
-  }
-
-}
-//обработчик на форму
-form.addEventListener('submit', dataForm);
+userList[0] = {
+  name: "Zuber1",
+  win: "211",
+  ko: "86",
+  looses: "112"
+};
+userList[1] = {
+  name: "Fireball67",
+  win: "251",
+  ko: "119",
+  looses: "82"
+};
+userList[2] = {
+  name: "Shot7",
+  win: "156",
+  ko: "130",
+  looses: "102"
+};
 
 
+//расчет птс
+const countPts = (arr) => {
+  arr.forEach(function (item,i,arr) {
+    //item.pts = (+item.win + +item.ko) - +item.looses;
+    arr[i].pts = (+arr[i].win + +arr[i].ko) - +arr[i].looses;
+  })
+};
 
 //сортировка по ПТС
 const sortTable = (arr) => {
   arr.sort((a, b) => a.pts < b.pts ? 1 : -1);
-}
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 //отрисовка таблицы с новыми значениями
 const createUser = (user) => {
@@ -116,7 +88,66 @@ const createUser = (user) => {
 
     tbody.append(clinetTr);
   }
+};
+
+//добавление нового игрока
+const addUser = (user) => {
+  userList.push(user);
+
+  countPts(userList);
+  sortTable(userList);
+  createUser(userList);
+
+  console.log(userList);
+};
+
+//получение данных с формы
+function dataForm(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const user = Object.fromEntries(formData.entries());
+  addUser(user);
+  event.target.reset();
 }
+
+//обработчик на форму
+form.addEventListener('submit', dataForm);
+
+
+
+
+
+
+
+
+countPts(userList);
+sortTable(userList);
+createUser(userList);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -124,7 +155,7 @@ const createUser = (user) => {
 
 
 //удаление игрока/строки
-table.addEventListener('click', (event) => {
+/* table.addEventListener('click', (event) => {
   event.preventDefault();
 
   const editClient = document.querySelectorAll('.edit');
@@ -140,7 +171,7 @@ table.addEventListener('click', (event) => {
       createUser(userList);
     }
     else if (event.target == (redactClient[i])) {
-      
+
       let userRow = editClient[i].parentNode;
       let redactList = userRow.childNodes;
 
@@ -168,43 +199,53 @@ table.addEventListener('click', (event) => {
       redactList[3].append(redactKo)
       redactList[4].append(redactLooses);
 
-      document.addEventListener('click', upDate(event, i));
-
       console.log(redactList);
     }
-    /* document.addEventListener('click', (event) => {
-     if (document.querySelector('.input-name')) {
-       if ((event.target !== redactName) && (event.target !== redactWin) && (event.target !== redactKo) && (event.target !== redactLooses)) {
-         userList[i].name = redactName.value;
-         userList[i].win = redactWin.value;
-         userList[i].ko = redactKo.value;
-         userList[i].looses = redactLooses.value;
-         createUser(userList);
-       }
-     }
-   }) */
   }
-
-
 })
 
+if (document.querySelector('.input-name')) {
+  document.addEventListener('click', (event) => {
+    if ((event.target !== redactName) && (event.target !== redactWin) && (event.target !== redactKo) && (event.target !== redactLooses)) {
+      userList[i].name = redactName.value;
+      userList[i].win = redactWin.value;
+      userList[i].ko = redactKo.value;
+      userList[i].looses = redactLooses.value;
+      createUser(userList);
+    }
+  })
+} */
 
-function upDate(event, i) {
 
+
+
+/* function upDate(i) {
   const redactName = document.querySelector('.input-name');
   const redactWin = document.querySelector('.input-win');
   const redactKo = document.querySelector('.input-ko');
   const redactLooses = document.querySelector('.input-looses');
+  document.addEventListener('click', (event) => {
+    if ((event.target !== redactName) && (event.target !== redactWin) && (event.target !== redactKo) && (event.target !== redactLooses)) {
+      userList[i].name = redactName.value;
+      userList[i].win = redactWin.value;
+      userList[i].ko = redactKo.value;
+      userList[i].looses = redactLooses.value;
+      //createUser(userList);
+      console.log("ffff");
+    }
+  });
+}
+ */
 
-  if ((event.target !== redactName) && (event.target !== redactWin) && (event.target !== redactKo) && (event.target !== redactLooses)) {
-    userList[i].name = redactName.value;
-    userList[i].win = redactWin.value;
-    userList[i].ko = redactKo.value;
-    userList[i].looses = redactLooses.value;
-    createUser(userList);
-    console.log("ffff");
+//addUser(userList);
+
+/* const validation = (form) => {
+
+  if ((+form.win >= 0) && (+form.ko >= 0) && (+form.looses >= 0) && (form.win >= form.ko) && (form.name.length > 0)) {
+    return true;
+  }
+  else {
+    return false;
   }
 
-}
-
-
+} */
